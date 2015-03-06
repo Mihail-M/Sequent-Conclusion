0a9d31dbc431a967d6d939b71d2d9efd5db9eea7
Mihail-M <Mihail-maryfich@yandex.ru>
2015-03-06 23:56:14 +0300

    Описание основных структур

---
+++ Sequent-calc/Program.fs
@@ -0,0 +1,14 @@
+﻿type Var = Var of string
+type Connective = And | Or | Implication | Not
+type Formula = Var | Op of Connective*Formula*Formula
+type Sequent = Sequent of Formula list * Formula list 
+type Side = Antecedent | Succedent
+
+type Label = Derivable | NotDerivable | Unknown
+type Tree<'a, 'b> =  Leaf of 'b 
+                    | Bend of 'a * Tree<'a, 'b> // возникает в случае однопосылочного вызова
+                    | Branch of 'a * Tree<'a, 'b> * Tree<'a,'b> // двухпосылочный
+
+type ProofTree = Tree of Sequent*(Sequent*Label) 
+
+
